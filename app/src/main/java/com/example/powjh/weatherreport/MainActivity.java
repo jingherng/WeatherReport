1c50bfa72e188e3ea900e8336d2fcf46b379823a
jh laptop <jingherng2010@gmail.com>
2018-09-04 23:13:41 +0800

    Initial Commit: testing Speech recognizer

---
+++ app/src/main/java/com/example/powjh/weatherreport/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.powjh.weatherreport;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity{
+
+    private final int REQUEST_SPEECH_RECOGNIZER = 3000;
+    private TextView mTextView;
+    private String mAnswer = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startVoice (View view){
+
+        // rng for different weather questions
+        int n = (int) (Math.random() * 4 );
+        String[] questions = new String[4];
+        questions[0] = "Where are you located at?";
+        questions[1] = "Tell me your location";
+        questions[2] = "Weather report at?";
+        questions[3] = "Where do you want your weather report at?";
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        // get default langauge of android device
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+        else
+            Toast.makeText(this, "Your Device Does Not Support Speech Input", Toast.LENGTH_SHORT).show();
+
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, questions[n]);
+        startActivityForResult(intent, REQUEST_SPEECH_RECOGNIZER);
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data!= null){
+            mAnswer = data.getStringExtra(RecognizerIntent.EXTRA_RESULTS);
+            mTextView.setText(mAnswer);
+        }
+    }
+}
